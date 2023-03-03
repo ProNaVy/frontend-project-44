4865a774c4936b9638950222b0b375e246a72d49
ProNaVy <prokopyeva_nv@mail.ru>
2023-03-03 14:04:20 +0300

    add bin/brain-games.js, package-lock.json, package.json, src/cli.js

---
+++ src/cli.js
@@ -0,0 +1,9 @@
+import readlineSync from 'readline-sync';
+
+const greetings = () => {
+
+const userName = readlineSync.question('May I have your name?');
+console.log('Hello, ' + userName + '!');
+};
+
+export default greetings
