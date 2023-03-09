6ff5843486dfb74cb1422d7c9a4d78a04004f188
ProNaVy <prokopyeva_nv@mail.ru>
2023-03-09 08:36:15 +0300

    add bin/brain-progression.js

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,16 +1,15 @@
-const arr = [];
-const progres = Math.floor(Math.random() * 10);
-console.log(progres);
+let arr = []
+  const progres = Math.floor(Math.random()*10)
+ 
 
-const firstElement = Math.floor(Math.random() * 20);
-let result = firstElement;
-let i;
-for (i = 0; i <= 10; i += 1) {
-  result += progres;
-  arr.push(result);
-}
-console.log(arr);
+  let firstElement = Math.floor(Math.random()*10)
+  let result = firstElement
+ for (i=0; i<=10; i+=1){
+  result = result+progres
+  arr.push(result)
+   }
 
-const ind = Math.floor(Math.random() * 10);
-arr.splice(ind, 1, '..');
-console.log(arr);
+let ind = Math.floor(Math.random()*10)
+let random = arr.splice(ind, 1, "..")
+
+  console.log(arr)
