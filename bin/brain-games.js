e4f02c9f1e02fe3d3e11f428f915a8493464d574
ProNaVy <prokopyeva_nv@mail.ru>
2023-03-17 18:47:15 +0300

    add bin/brain-games.js, bin/brain-progression.js, bin/games.js, package-lock.json, src/games/brain-progression copy.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -3,8 +3,7 @@ import greetings from '../src/cli.js';
 import brainEven from './brain-even.js';
 import brainCalc from './brain-calc.js';
 import brainGcd from './brain-gcd.js';
-import brainProgression from './brain-progression.js';
-
+import { brainProgression } from './brain-progression.js';
 
 greetings();
 brainEven();
--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,17 +1,47 @@
 import readlineSync from 'readline-sync';
+import { game } from './games.js';
 
+console.log('Welcome to the Brain Games!');
 const userName = readlineSync.question('May I have your name?');
+console.log(`Hello, ${userName}!`);
 
-const brainProgression = () => {
+export const brainProgression = () => {
   console.log('Brain progression');
-
   console.log('What number is missing in the progression?');
-  const game = () => {
+
+  // инициализация массива с прогрессией
+  const arr = [];
+  const progres = Math.floor(Math.random() * 10);
+
+  const firstElement = Math.floor(Math.random() * 10);
+  let result = firstElement;
+  let i;
+  for (i = 0; i <= 10; i += 1) {
+    result += progres;
+    arr.push(result);
+  }
+  const ind = Math.floor(Math.random() * 10);
+  arr.splice(ind, 1, '..');
+
+  const question = arr;
+
+  // правильный ответ
+
+  const trueAnsver = (ind === 0 ? firstElement + progres : arr[ind - 1] + progres);
+
+  // запуск игры
+
+  game(question, trueAnsver, userName);
+};
+
+
+
+/* const game = () => {
     let i;
     let res;
     for (i = 0; i < 3; i += 1) {
       const arr = [];
-   
+
       const progres = Math.floor(Math.random() * 10);
       const firstElement = Math.floor(Math.random() * 10);
       let result = firstElement;
@@ -19,26 +49,25 @@ const brainProgression = () => {
         result += progres;
         arr.push(result);
       };
-  
+
       const ind = Math.floor(Math.random() * 10);
       arr.splice(ind, 1, '..');
-  
-  
+
       const answer = readlineSync.question(`Question: ${arr} \nYour answer:`);
 
-      const trueAnsver = () => (ind === 0 ? firstElement + progres : arr[ind - 1] + progres);
+      const trueAnsver = (ind === 0 ? firstElement + progres : arr[ind - 1] + progres);
 
-      if (trueAnsver() === Number(answer)) {
+      if (trueAnsver === Number(answer)) {
      console.log('Correct!');
       } else {
-    console.log(`'${answer}' is wrong answer ;(. Correct answer was '${trueAnsver()}'. Let's try again, ${userName}!`);
+    console.log(`'${answer}' is wrong answer ;(. Correct answer was '${trueAnsver}'. Let's try again, ${userName}!`);
         break;
       }
     }
     if (i === 3) {console.log(`Congratulations, ${userName}!`)};
-  
+
   };
   game();
 };
 
-export default brainProgression
+export default brainProgression */
