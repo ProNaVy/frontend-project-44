ad4326e6d3928d992aaa87ae78b22cc71bd9cf06
ProNaVy <prokopyeva_nv@mail.ru>
2023-03-26 12:27:53 +0300

    add package.json, src/games/brainPrime.js

--- src/games/brainPrime.js
+++ src/games/brainPrime.js
@@ -7,8 +7,7 @@ export const brainPrime = (name) => {
   const round = () => {
     const question = Math.floor(Math.random() * 100);
 
-    // проверка числа на удовлетворение условия
-    const primeNumb = (num) => {
+       const primeNumb = (num) => {
       if (num > 1) {
         for (let a = 2; a < num; a += 1) {
           if (num % a === 0) {
@@ -20,9 +19,7 @@ export const brainPrime = (name) => {
       return null;
     };
 
-    // правильный ответ
-
-    const trueAnsver = (primeNumb(question) === true ? 'yes' : 'no');
+     const trueAnsver = (primeNumb(question) === true ? 'yes' : 'no');
 
     return [question, trueAnsver];
   };
